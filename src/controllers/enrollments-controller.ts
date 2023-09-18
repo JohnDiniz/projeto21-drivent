11d80672eaf240666a2d553a94525d417bb0044e
João Vitor Diniz Mistrinel <abissaldev@gmail.com>
2023-09-18 09:00:02 -0300

    test

--- src/controllers/enrollments-controller.ts
+++ src/controllers/enrollments-controller.ts
@@ -4,33 +4,25 @@ import { AuthenticatedRequest } from '@/middlewares';
 import { enrollmentsService } from '@/services';
 
 export async function getEnrollmentByUser(req: AuthenticatedRequest, res: Response) {
-  const { userId } = req;
+    const { userId } = req;
 
-  const enrollmentWithAddress = await enrollmentsService.getOneWithAddressByUserId(userId);
+    const enrollmentWithAddress = await enrollmentsService.getOneWithAddressByUserId(userId);
 
-  return res.status(httpStatus.OK).send(enrollmentWithAddress);
+    return res.status(httpStatus.OK).send(enrollmentWithAddress);
 }
 
 export async function postCreateOrUpdateEnrollment(req: AuthenticatedRequest, res: Response) {
-  await enrollmentsService.createOrUpdateEnrollmentWithAddress({
-    ...req.body,
-    userId: req.userId,
-  });
+    await enrollmentsService.createOrUpdateEnrollmentWithAddress({
+        ...req.body,
+        userId: req.userId,
+    });
 
-  return res.sendStatus(httpStatus.OK);
+    return res.sendStatus(httpStatus.OK);
 }
 
 // TODO - Receber o CEP do usuário por query params.
 export async function getAddressFromCEP(req: AuthenticatedRequest, res: Response) {
-  try {
-    const { cep } = req.query;
-    if (!cep || typeof cep !== 'string') {
-      return res.status(httpStatus.BAD_REQUEST).json({ error: 'CEP inválido' });
-    }
+    const cep = req.query.cep && String(req.query.cep);
     const address = await enrollmentsService.getAddressFromCEP(cep);
     res.status(httpStatus.OK).send(address);
-  } catch (error) {
-    console.error('Erro ao buscar informações de endereço:', error);
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ error: 'Erro interno do servidor' });
-  }
 }
