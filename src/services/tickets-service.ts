793bc84aba92fd5cc9abbef258e16d2877edf9e9
João Vitor Diniz Mistrinel <abissaldev@gmail.com>
2023-09-25 19:28:52 -0300

    fix: :sparkles: remove types in return

--- src/services/tickets-service.ts
+++ src/services/tickets-service.ts
@@ -3,16 +3,7 @@ import { TicketType, Ticket } from '@/protocols';
 
 export async function getFormattedTicketTypes(): Promise<TicketType[]> {
   const ticketTypes = await findAllTicketTypes();
-
-  return ticketTypes.map((type) => ({
-    id: type.id,
-    name: type.name,
-    price: type.price,
-    isRemote: type.isRemote,
-    includesHotel: type.includesHotel,
-    createdAt: type.createdAt,
-    updatedAt: type.updatedAt,
-  }));
+  return ticketTypes;
 }
 
 export async function findUserTicket(userId: number): Promise<Ticket | null> {
@@ -29,21 +20,5 @@ export async function createTicketService(userId: number, ticketTypeId: number) 
 
   const newTicket = await createNewTicket(ticketTypeId, enrollment.id);
 
-  return {
-    id: newTicket.id,
-    status: newTicket.status,
-    ticketTypeId: newTicket.ticketTypeId,
-    enrollmentId: newTicket.enrollmentId,
-    TicketType: {
-      id: newTicket.TicketType.id,
-      name: newTicket.TicketType.name,
-      price: newTicket.TicketType.price,
-      isRemote: newTicket.TicketType.isRemote,
-      includesHotel: newTicket.TicketType.includesHotel,
-      createdAt: newTicket.TicketType.createdAt,
-      updatedAt: newTicket.TicketType.updatedAt,
-    },
-    createdAt: newTicket.createdAt,
-    updatedAt: newTicket.updatedAt,
-  };
+  return newTicket;
 }
