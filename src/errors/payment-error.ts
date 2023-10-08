d4b11ea5d37d59b948ea38524cd54be2cb2ce5e1
JohnDiniz <abissaldev@gmail.com>
2023-10-08 14:22:50 -0300

    .

--- src/errors/payment-error.ts
+++
@@ -1,9 +0,0 @@
-import { RequestError } from '@/protocols';
-
-export function PaymentError(message: string, status: number): RequestError {
-  return {
-    name: 'PaymentError',
-    message,
-    status,
-  };
-}
